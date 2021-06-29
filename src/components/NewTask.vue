38ac734ec1c02d0e5c5a43ddcce841304ab2aabe
nicolastavs <nico.bt19@gmail.com>
2021-06-28 22:44:54 -0300

    second commit!

---
+++ src/components/NewTask.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="new-task">
+    <input
+      v-model="name"
+      @keydown.enter="add"
+      type="text"
+      class="form-element"
+      placeholder="New task?"
+    />
+    <button class="form-element" @click="add">+</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: "",
+    };
+  },
+  methods: {
+    add() {
+      this.$emit("newTask", { name: this.name });
+      this.name = "";
+    },
+  },
+};
+</script>
+
+ <style>
+.new-task {
+  margin: 35px;
+}
+
+.form-element {
+  outline: none;
+  font-size: 1, 7rem;
+  border: 2px solid white;
+  padding: 5px 10px 8px;
+  color: white;
+}
+
+input.form-element {
+  width: 500px;
+  background: #fff2;
+  border-top-left-radius: 8px;
+  border-bottom-left-radius: 8px;
+}
+
+button.form-element {
+  border-left: none;
+  background-color: darkgrey;
+  border-top-right-radius: 8px;
+  border-bottom-right-radius: 8px;
+}
+</style>
+ 
---
+++ src/components/Task.vue
@@ -0,0 +1,69 @@
+<template>
+  <div
+    @click="$emit('taskStateChanged', task)"
+    class="task"
+    :class="stateClass"
+  >
+    <span @click.stop="$emit('taskDeleted', task)" class="close">x</span>
+    <p>{{ task.name }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    task: { type: Object, required: true },
+  },
+  computed: {
+    stateClass() {
+      return {
+        pending: this.task.pending,
+        done: !this.task.pending,
+      };
+    },
+  },
+};
+</script>
+
+<style>
+.task {
+  box-sizing: border-box;
+  position: relative;
+  width: 350px;
+  height: 150px;
+  padding: 10px;
+  border-radius: 8px;
+  font-size: 2rem;
+  font-weight: 300;
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.pending {
+  border-left: 12px solid rgb(143, 0, 143);
+  background-color: rgb(202, 0, 202);
+}
+
+.done {
+  border-left: 12px solid rgb(82, 0, 82);
+  background-color: purple;
+  text-decoration: line-through;
+}
+
+.close {
+  background-color: black;
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  border-radius: 8px;
+  font-size: 0.9rem;
+  font-weight: 600;
+  height: 20px;
+  width: 20px;
+  border-radius: 10px;
+  display: flex;
+  justify-content: center;
+}
+</style>
